204dc33f9206266555d6fc457e05a45807e76c66
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-10 12:14:17 -0300

    shore: about.tsx

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -21,9 +21,12 @@ const About = () => {
 
             <h6>  Sou um desenvolvedor front-end!  </h6>
 
-            <p className={styles.coment}> Apaixonado pela área de tecnologia, e tenho certeza que posso contribuir em qualquer time com as minhas habilidades! </p>
+            <p className={styles.coment} >Desenvolvedor apaixonado por tecnologia, sempre em busca de desafios e aprendizado contínuo.
+                <br/> Atuo em comunidades de programação, trocando feedbacks e compartilhando conhecimento para crescer junto com outros profissionais. <br/> Pronto para contribuir de forma significativa em qualquer equipe!</p>
+
+            {/* <p className={styles.coment}> Apaixonado pela área de tecnologia, e tenho certeza que posso contribuir em qualquer time com as minhas habilidades! </p>
             <p className={styles.coment}> Estou sempre me desafiando com novos projetos e participando de comunidades de programação para buscar feedback de outros desenvolvedores. Além de tentar ajudar essas pessoas com o que eu já aprendi!</p>
-            <p className={styles.coment}> Atualmente estou estudando tecnologias frontend, como HTML5, CSS3, JavaScript, TypeScript, React e <strong>buscando uma oportunidade no mercado profissional</strong>, para colocar em prática os conhecimentos adquiridos. Observo o mercado profissional da área, estudando as novas tecnologias para me manter atualizado! </p>
+            <p className={styles.coment}> Atualmente estou estudando tecnologias frontend, como HTML5, CSS3, JavaScript, TypeScript, React e <strong>buscando uma oportunidade no mercado profissional</strong>, para colocar em prática os conhecimentos adquiridos. Observo o mercado profissional da área, estudando as novas tecnologias para me manter atualizado! </p> */}
             <button onClick={handleLink}  rel='noopener noreferrer' >
                 <svg>
                     <rect x="0" y="0" width="100%" height="100%" />
